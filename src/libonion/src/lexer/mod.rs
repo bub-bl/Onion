caeb93a181c6689eebcc5badd50d378b165ebac9
bub.bl <66354328+bub-bl@users.noreply.github.com>
2023-12-30 16:25:28 +0100

    Lexer base with basic implementation

--- src/libonion/src/lexer/mod.rs
+++ src/libonion/src/lexer/mod.rs
@@ -1,1 +1,34 @@
-mod token;
+pub mod token;
+
+pub struct Lexer<'a> {
+    input: &'a str,
+    position: usize,
+    read_position: usize,
+    ch: char,
+}
+
+impl<'a> Lexer<'a> {
+    pub fn new(input: &'a str) -> Self {
+        let mut l = Lexer {
+            input, 
+            position: 0, 
+            read_position: 0, 
+            ch: '\0'
+        };
+
+        l.read_char();
+        return l;
+    }
+
+    fn read_char(&mut self) {
+        if self.read_position >= self.input.len() {
+            self.ch = 0 as char
+        } else {
+            if let Some(ch) = self.input.chars().nth(self.read_position) {
+                self.ch = ch;
+            } else {
+                panic!("Read out of bounds");
+            }
+        }
+    }
+}
