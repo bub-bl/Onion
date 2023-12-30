2fd55bf3580a04203b5be564e38e641479b5ef28
bub.bl <66354328+bub-bl@users.noreply.github.com>
2023-12-30 16:19:52 +0100

    Add some default tokens + formatter

---
+++ src/libonion/src/lexer/token.rs
@@ -0,0 +1,114 @@
+use std::fmt;
+
+#[derive(Clone, Debug, Eq, Hash, Ord, PartialOrd, PartialEq)]
+pub struct Span {
+    pub start: usize,
+    pub end: usize
+}
+
+#[derive(Clone, Debug, Eq, Hash, Ord, PartialOrd, PartialEq)]
+pub struct Token {
+    pub kind: TokenKind,
+    pub span: Span,
+}
+
+impl fmt::Display for Token {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "start: {}, end: {}, kind: {}", self.span.start, self.span.end, self.kind)
+    }
+}
+
+#[derive(Clone, Debug, Eq, Hash, Ord, PartialOrd, PartialEq)]
+pub enum TokenKind {
+    Illegal,
+    EOF,
+
+    Identifier(String),
+    Integer(i64),
+
+    Assign,
+    Plus,
+    Minus,
+    Bang,
+    Asterisk,
+    Slash,
+
+    LessThan,
+    GreaterThan,
+    LessThanEqual,
+    GreaterThanEqual,
+
+    Equal,
+    NotEqual,
+
+    Comma,
+    Semicolon,
+    Colon,
+
+    LParen,
+    RParen,
+    LBrace,
+    RBrace,
+    LBracket,
+    RBracket,
+
+    Function,
+    Let,
+    True,
+    False,
+    If,
+    Else,
+    Return,
+}
+
+impl fmt::Display for TokenKind {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            TokenKind::Illegal => write!(f, "ILLEGAL"),
+            TokenKind::EOF => write!(f, "EOF"),
+            TokenKind::Identifier(ident) => write!(f, "{}", ident),
+            TokenKind::Integer(i) => write!(f, "{}", i),
+            TokenKind::Assign => write!(f, "="),
+            TokenKind::Plus => write!(f, "+"),
+            TokenKind::Minus => write!(f, "-"),
+            TokenKind::Asterisk => write!(f, "*"),
+            TokenKind::Slash => write!(f, "/"),
+            TokenKind::Bang => write!(f, "!"),
+            TokenKind::LessThan => write!(f, "<"),
+            TokenKind::GreaterThan => write!(f, ">"),
+            TokenKind::LessThanEqual => write!(f, "<="),
+            TokenKind::GreaterThanEqual => write!(f, ">="),
+            TokenKind::Equal => write!(f, "=="),
+            TokenKind::NotEqual => write!(f, "!="),
+            TokenKind::Comma => write!(f, ","),
+            TokenKind::Semicolon => write!(f, ";"),
+            TokenKind::Colon => write!(f, ":"),
+            TokenKind::LParen => write!(f, "("),
+            TokenKind::RParen => write!(f, ")"),
+            TokenKind::LBrace => write!(f, "{{"),
+            TokenKind::RBrace => write!(f, "}}"),
+            TokenKind::LBracket => write!(f, "["),
+            TokenKind::RBracket => write!(f, "]"),
+            TokenKind::Function => write!(f, "fn"),
+            TokenKind::Let => write!(f, "let"),
+            TokenKind::True => write!(f, "true"),
+            TokenKind::False => write!(f, "false"),
+            TokenKind::If => write!(f, "if"),
+            TokenKind::Else => write!(f, "else"),
+            TokenKind::Return => write!(f, "return"),
+        }
+    }
+}
+
+pub fn lookup_identifier(identifier: &str) -> TokenKind {
+    match identifier {
+        "fn" => TokenKind::Function,
+        "let" => TokenKind::Let,
+        "true" => TokenKind::True,
+        "false" => TokenKind::False,
+        "if" => TokenKind::If,
+        "else" => TokenKind::Else,
+        "return" => TokenKind::Return,
+        _ => TokenKind::Identifier(identifier.to_string()),
+    }
+}
